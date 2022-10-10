65ee0d480e35c9de99f5ad7c241070b798ce2431
rakesh635 <rakesh635@gmail.com>
2022-10-10 16:42:24 +0530

    Update package-projects.sh

--- package-projects.sh
+++ package-projects.sh
@@ -3,11 +3,11 @@
 set -ex
 
 mvn -DskipTests=true clean install
-mvn -f ./remainder-service/pom.xml -Pdockerimage docker:build 
-mvn -f ./eureka-server/pom.xml  -Pdockerimage docker:build 
-mvn -f ./config-server/pom.xml  -Pdockerimage docker:build 
-mvn -f ./oauth-server/pom.xml  -Pdockerimage docker:build 
-mvn -f ./user-service/pom.xml  -Pdockerimage docker:build 
-mvn -f ./api-gateway/pom.xml  -Pdockerimage docker:build 
-mvn -f ./admin-server/pom.xml  -Pdockerimage docker:build 
-mvn -f ./mail-service/pom.xml  -Pdockerimage docker:build
+mvn -DskipTests=true -f ./remainder-service/pom.xml -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./eureka-server/pom.xml  -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./config-server/pom.xml  -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./oauth-server/pom.xml  -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./user-service/pom.xml  -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./api-gateway/pom.xml  -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./admin-server/pom.xml  -Pdockerimage docker:build 
+mvn -DskipTests=true -f ./mail-service/pom.xml  -Pdockerimage docker:build
